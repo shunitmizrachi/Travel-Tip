5184594bb17db69d42d0871244e560a920c9c59d
Shunit Mizrachi <shuna1800@gmail.com>
2022-02-23 17:57:54 +0200

    edited local storage file

--- js/services/storage.service.js
+++ js/services/storage.service.js
@@ -15,13 +15,3 @@ function loadFromStorage(key) {
 }
 
 
-function saveToStorage(key, val) {
-    const str = JSON.stringify(val)
-    localStorage.setItem(key, str)
-}
-
-function loadFromStorage(key) {
-    const str = localStorage.getItem(key)
-    const val = JSON.parse(str)
-    return val
-}
