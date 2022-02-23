e0124a58a76a75e49500972f381046c43f95872c
chenbezalel <chenbezalel55@gmail.com>
2022-02-23 15:11:25 +0200

    add api key to map.service

--- js/services/map.service.js
+++ js/services/map.service.js
@@ -40,7 +40,7 @@ function panTo(lat, lng) {
 
 function _connectGoogleApi() {
     if (window.google) return Promise.resolve()
-    const API_KEY = ''; //TODO: Enter your API Key
+    const API_KEY = 'AIzaSyBDOoiFFGSeIYY1Cgon8OTLv-kiluzSie8'; //TODO: Enter your API Key
     var elGoogleApi = document.createElement('script');
     elGoogleApi.src = `https://maps.googleapis.com/maps/api/js?key=${API_KEY}`;
     elGoogleApi.async = true;
